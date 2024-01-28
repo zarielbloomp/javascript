253439c1266d9fe947366bc6a41468699d26f3ac
zarielbloomp <miguel.c.nicoli@gmail.com>
2024-01-28 17:26:44 -0300

    cabei

--- modD/DESAFIO002/script.js
+++ modD/DESAFIO002/script.js
@@ -16,28 +16,39 @@ function enviar() {
            genero = 'Homem'
            if(idade >=0 && idade <12){
             //criança
+            img.setAttribute('src','img/bebe-H.jpg')
            }else if( idade >=12 && idade < 21 ){
             //jovem
+            img.setAttribute('src','img/jovem-H.jpg')
            }else if(idade >=21 && idade <60){
             //adulto
+            img.setAttribute('src','img/adulto-H.jpg')
            }else{
-            idoso
+            //idoso
+            img.setAttribute('src','img/idoso-H.jpg')
            }
            res.innerHTML = `Detectamos um ${genero} com ${idade} anos de idade` 
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if(idade >=0 && idade <12){
                 //criança
+                img.setAttribute('src','img/bebe-M.jpg')
                }else if( idade >=12 && idade < 21 ){
                 //jovem
+                img.setAttribute('src','img/jovem-M.jpg')
                }else if(idade >=21 && idade <60){
                 //adulto
+                img.setAttribute('src','img/adulta-M.jpg')
                }else{
-                idoso
+                //idoso
+                img.setAttribute('src','img/idosa-M.jpg')
                }
             res.innerHTML = `Detectamos uma ${genero} com ${idade} anos de idade`
+            
         }
         
     }
+    res.style.textAlign = 'center'
+    res.appendChild(img)
 }
 
